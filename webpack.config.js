7bd465bca03be1873062fbb47ac336609f7c569e
Crystal Pederson <88516047+crystalpederson@users.noreply.github.com>
2021-12-14 07:19:55 -0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack'); // DF added
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+//   entry: path.join(__dirname, 'client', 'index'),
+  entry: path.join(__dirname, './client/index.js'),
+  output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+    },
+  mode: process.env.NODE_ENV,
+  module: {
+      rules: [
+          {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options:{
+                      presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+              }
+          },
+          {
+              test: /\.s[ac]ss$/i,
+              use: [
+                  // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+              ]
+          }
+      ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './index.html',
+  })],
+  devServer: {
+      static: {
+          publicPath: '/build',
+          directory: path.join(__dirname, 'client')
+      },  
+      proxy: {
+        '/api': 'http://localhost:3000'
+      },
+  },
+
+};
